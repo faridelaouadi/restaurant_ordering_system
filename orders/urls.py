ef4fa2b387bd91cff635b669fdaa4268e2795903
Farid El-Aouadi <faridelaouadi@hotmail.co.uk>
2019-11-28 17:52:31 +0000

    added login/logout/register functionality

--- orders/urls.py
+++ orders/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = "orders"
+
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("login", views.login_request, name="login"),
+    path("register/", views.register, name="register"),
+    path("logout", views.logout_request, name="logout"),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,53 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Category
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import logout, authenticate, login
 
 # Create your views here.
 def index(request):
-    #we are passing in the data from the category model
-    return render(request, "orders/home.html", {"categories":Category.objects.all})
+    if request.user.is_authenticated:
+        #we are passing in the data from the category model
+        return render(request, "orders/home.html", {"categories":Category.objects.all})
+    else:
+        return redirect("orders:login")
+
+def register(request):
+    return HttpResponse("this is the register page")
+
+def login_request(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+
+    form = AuthenticationForm()
+    return render(request = request,
+                    template_name = "orders/login.html",
+                    context={"form":form})
+
+def logout_request(request):
+    logout(request)
+    return redirect("orders:login")
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            login(request, user)
+            return redirect("orders:index")
+
+        return render(request = request,
+                          template_name = "orders/register.html",
+                          context={"form":form})
+
+    return render(request = request,
+                  template_name = "orders/register.html",
+                  context={"form":UserCreationForm})
