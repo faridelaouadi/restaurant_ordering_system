33245e85a95b5f03f4eaf9e007759c4d3297294e
Farid El-Aouadi <faridelaouadi@hotmail.co.uk>
2019-12-03 17:09:54 +0000

    added notifications when the user adds to cart

--- orders/static/orders/main.js
+++ orders/static/orders/main.js
@@ -1,5 +1,29 @@
 function add_to_cart(info){
   //info will be the stuff displayed in the reciept
   // item description as well as teh price
-  console.log(info)
+  display_notif(info);
+  
+  //console.log(info.item_description)
+  //console.log(info.price)
+}
+
+function display_notif(info){
+  toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": true,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "70",
+    "hideDuration": "1000",
+    "timeOut": "2000",
+    "extendedTimeOut": "500",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+  }
+  toastr.success(info.item_description + ': £' + info.price, 'Added to Cart');
 }
