cbd553904e8d11be6ed416eb2b6d5c388f356392
Farid El-Aouadi <faridelaouadi@hotmail.co.uk>
2019-12-03 18:37:57 +0000

    cart now updating yay

--- orders/static/orders/main.js
+++ orders/static/orders/main.js
@@ -1,10 +1,58 @@
+$(document).ready(function() {
+    if (window.location.href.indexOf("cart") > -1) {
+      //if we are on the cart page
+      //dynamically generate the cart on the page
+      var cart = JSON.parse(localStorage.getItem("cart"));
+      var total = 0;
+      var table = document.getElementById('cart_body');
+
+      for (var i = 0; i < cart.length; i++) {
+
+        var tr = document.createElement('tr');
+        var th = document.createElement('th');
+        var td1 = document.createElement('td');
+        var td2 = document.createElement('td');
+
+        var text1 = document.createTextNode(cart[i].item_description);
+        var text2 = document.createTextNode(cart[i].price);
+        var text3 = document.createTextNode(String(i+1));
+
+        td1.appendChild(text1);
+        td2.appendChild(text2);
+        th.appendChild(text3)
+        tr.appendChild(th);
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+
+        table.appendChild(tr);
+
+        total += cart[i].price
+      }
+      document.getElementById('total').innerHTML = '£' + String(total);
+
+
+
+
+    }
+  });
+
 function add_to_cart(info){
   //info will be the stuff displayed in the reciept
   // item description as well as teh price
   display_notif(info);
-  
-  //console.log(info.item_description)
-  //console.log(info.price)
+  var cart_retrieved = localStorage.getItem("cart")
+  if (cart_retrieved === null){
+    //make a new cart
+    var cart = [info];
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }else{
+    var cart = JSON.parse(cart_retrieved);
+    cart.push(info)
+    console.log("new cart would be ---> "+ cart);
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }
+
+
 }
 
 function display_notif(info){
@@ -27,3 +75,14 @@ function display_notif(info){
   }
   toastr.success(info.item_description + ': £' + info.price, 'Added to Cart');
 }
+
+
+
+
+
+/*
+<tr>
+  <th scope="row">{{ forloop.counter }}</th>
+  <td>Mark</td>
+  <td>Otto</td>
+</tr>*/
